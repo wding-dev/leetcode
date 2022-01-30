17328dad82317fa1857ce06c0b071081cd611b6b
wding-dev <weiding_dev@sina.com>
2022-01-30 23:19:39 +1100

    Reformat code

--- java/src/main/java/TrappingRainWater.java
+++ java/src/main/java/TrappingRainWater.java
@@ -1,20 +1,18 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TrappingRainWater {
-    private static final Logger LOGGER = Logger.getLogger( TrappingRainWater.class.getName() );
+    private static final Logger LOGGER = Logger.getLogger(TrappingRainWater.class.getName());
 
     public static void main(String[] args) {
-        int[] nums = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
+        int[] nums = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
 
         int result = solution(nums);
 
         if (result == 6) {
-            LOGGER.log( Level.INFO, "Answer {0}", "correct");
+            LOGGER.log(Level.INFO, "Answer {0}", "correct");
         } else {
-            LOGGER.log( Level.SEVERE, "Answer {0}", "incorrect");
+            LOGGER.log(Level.SEVERE, "Answer {0}", "incorrect");
         }
     }
 
